570587da436daca412ed469828eed861a7badcb1
thequbit <thequbit@gmail.com>
2014-09-24 21:39:44 -0400

    added support for multiple threads

--- yapot/yapot.py
+++ yapot/yapot.py
@@ -1,6 +1,16 @@
 from wand.image import Image as WandImage
 import subprocess
 import os
+from multiprocessing import Queue
+from multiprocessing import Pool
+
+import time
+
+from pyPdf import PdfFileWriter
+from pyPdf import PdfFileReader
+
+__pdf_queue = Queue()
+__pdf_images = Queue()
 
 def convert_document(pdf_filename, base_page_name='page', resolution=200,
         delete_files = True, page_delineation='\n--------\n',
@@ -19,6 +29,7 @@ def convert_document(pdf_filename, base_page_name='page', resolution=200,
         imgs = _get_images_from_pdf(
             pdf_filename = pdf_filename,
             resolution = resolution,
+            base_page_name = base_page_name,
         )
 
         if verbose:
@@ -26,9 +37,13 @@ def convert_document(pdf_filename, base_page_name='page', resolution=200,
 
         # wand makes image sequences out of multi-page files
         page_count = len(imgs)
-        for i,im in imgs:
-            success, image_filename = _save_page_image(i,im,base_page_name)
-            if success == True:
+        completed_pages = 0
+        for image_filename in imgs:
+            #success, image_filename = _save_page_image(i,im,base_page_name)
+            #if success == True:
+                print "\n\n"
+                print image_filename
+                print "\n\n"
                 success, page_text = _convert_image_to_text(image_filename)
                 if success == True:
                     if delete_files == True:
@@ -40,6 +55,8 @@ def convert_document(pdf_filename, base_page_name='page', resolution=200,
 
                 output_text = "{0}{1}{2}".format(output_text, page_text, page_delineation)
 
+                completed_pages += 1
+
                 if verbose:
                     print "Successfully converted page %i of %i." % (i,page_count)
 
@@ -50,22 +67,121 @@ def convert_document(pdf_filename, base_page_name='page', resolution=200,
 
     return success, output_text
 
-def _get_images_from_pdf(pdf_filename, resolution=200):
+def _get_images_from_pdf(pdf_filename, resolution=200, base_page_name='page'):
 
-    ret_imgs = None
+    ret_images = []
+    #__pdf_images = []
     #try:
     if True:
-        print "Starting pdf read ..."
-        imgs = WandImage(
-            filename=pdf_filename,
-            resolution=resolution
+
+        print "Splitting PDF into multiple pdf's for processing ..."
+
+        # make sure there is a place to put our temporary pdfs
+        if not os.path.exists('temp'):
+            os.makedirs('temp')
+
+        # read input pdf
+        inputpdf = PdfFileReader(open(pdf_filename, "rb"))
+        if inputpdf.getIsEncrypted():
+            inputpdf.decrypt('')
+
+        print "Writing out %i pages ..." % inputpdf.numPages
+
+        # create all of the temporary pdfs
+        for i in range(0,16): #xrange(inputpdf.numPages):
+            output = PdfFileWriter()
+            output.addPage(inputpdf.getPage(i))
+            filename = "temp/document-page-%s.pdf" % i
+            with open(filename, "wb") as outputStream:
+                output.write(outputStream)
+            __pdf_queue.put(i)
+            ret_images.append(None)
+
+        #print __pdf_images[0]
+
+        #print "Queue holds {0} pdfs, to be converted into {1} images.".format(
+        #    __pdf_queue.qsize(),
+        #    len(__pdf_images),
+        #)
+
+        print "Dispatching pdf workers ..."
+
+        # spin up our workers to convert the pdfs to images
+        pool_count = 8
+        pool = Pool()
+        result = pool.map_async(
+            _pdf_converter_worker, 
+            [(x, resolution, base_page_name) for x in range(pool_count)]
         )
-        print "Combining images ..."
-        ret_imgs = zip(xrange(len(imgs.sequence)), imgs.sequence)
+
+        while not __pdf_queue.empty():
+            time.sleep(.25)
+        
+        #if True:
+        try:
+            while(1):
+                image_number,image_filename = __pdf_images.get_nowait()
+                ret_images[image_number] = image_filename
+        except:
+            pass
+
+        #print __pdf_images
+
+        print ret_images
+
+        print "Done conerting PDF."
+
     #except:
     #    pass
 
-    return ret_imgs
+    return ret_images
+
+def _pdf_converter_worker(args):
+
+    thread_number = args[0]
+    resolution = args[1]
+    base_page_name = args[2]
+
+    #if True:
+    try:
+
+        while(1):
+
+            #print "{0}: Getting page from queue ...".format(thread_number)
+
+            page_number = __pdf_queue.get_nowait()
+
+            print "{0}: working on page {1}".format(thread_number, page_number)
+
+            imgs = WandImage(
+                filename="temp/document-page-{0}.pdf".format(page_number),
+                resolution=resolution
+            )
+
+            #print "{0}: done with page {1}".format(thread_number, page_number)
+
+            ret_imgs = zip(xrange(len(imgs.sequence)), imgs.sequence)
+
+            #print "{0}: saving image data ...".format(thread_number)
+
+            _i, _im = ret_imgs[0]
+            success, image_filename = _save_page_image(page_number, _im, base_page_name)
+
+            #print "{0}: saving image {1} filename to list ...".format(thread_number, page_number)
+
+            __pdf_images.put((int(page_number), image_filename))
+
+            #print "{0}: done.".format(thread_number)
+
+            #__pdf_queue.task_done()
+
+    except Exception, e:
+    #    pass
+        print str(e)
+
+    return 
+
+    
 
 def _save_page_image(page_number, image, base_page_name='page'):
 
@@ -85,6 +201,8 @@ def _save_page_image(page_number, image, base_page_name='page'):
 
 def _convert_image_to_text(image_filename):
 
+    print "Converting {0} ...".format(image_filename)
+
     success = False
     page_text = ''
     #try:
@@ -99,6 +217,8 @@ def _convert_image_to_text(image_filename):
         success = True
     #except:
     #    pass
+
+    print "Done."
 
     return success, page_text
 
